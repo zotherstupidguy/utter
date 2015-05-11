c8df1b149088b84f712ffa9c7e3854f51e927fd7
zotherstupidguy <zotherstupidguy@gmail.com>
2015-05-11 06:52:03 +0200

    Update api_spec.rb

--- spec/api_spec/api_spec.rb
+++ spec/api_spec/api_spec.rb
@@ -1,7 +1,23 @@
-require_relative './helper'
+require_relative './../helper'
+require 'net/http'
+
 class S < Utter::Endpoint
   get "/" do
     "hello"
-#    log.info "huh"
   end
+end
+run Utter::Endpoints
+
+describe 'Utter API' do 
+  before  do 
+    uri = URI('http://127.0.0.1:8080')
+    #params = { :limit => 10, :page => 3 }
+    #uri.query = URI.encode_www_form(params)
+    @res = Net::HTTP.get_response(uri)
+  end
+
+  it "assert somthing" do 
+    @res.body.must_equal nil #Net::HTTPSuccess
+  end
+
 end
