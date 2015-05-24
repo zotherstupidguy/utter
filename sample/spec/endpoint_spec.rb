cb08ad168b10194d43a7ada7c499c266841ccdb4
zotherstupidguy <zotherstupidguy@gmail.com>
2015-05-24 10:23:21 +0200

    Adds sample/spec/endpoint_spec

--- sample/spec/endpoint_spec.rb
+++ sample/spec/endpoint_spec.rb
@@ -1,1 +1,22 @@
 # Specs for the endpoint
+require_relative './helper'
+require_relative './../lib/services/v1/endpoint'
+
+describe 'Endpoint' do
+  before do
+    def path url
+      return `curl http://127.0.0.1:9292/api/v1/services#{url}`
+    end
+  end
+
+  describe "Services::V1::Endpoint" do
+    it "hi" do
+      path("/hi?user=hero").must_be_kind_of Hash #"hero" #{ "hello" => "hero" } #.to_json
+    end
+
+    it "it plays pingpong" do 
+      path("/ping").must_equal "pong"
+    end
+
+  end
+end
